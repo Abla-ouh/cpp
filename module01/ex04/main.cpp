b1f76638679f49cc06758ee11bb02ea3ddf738ec
Abla-ouh <Ouhagaabla@gmail.com>
2022-12-07 17:05:19 +0100

    ex04 ONGOING

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abouhaga <abouhaga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 11:04:05 by abouhaga          #+#    #+#             */
-/*   Updated: 2022/12/05 11:24:42 by abouhaga         ###   ########.fr       */
+/*   Updated: 2022/12/05 12:16:53 by abouhaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,24 @@
 
 int main(int argc, char** argv)
 {
+    if (argc != 4 || *argv[2] == '\0' || *argv[3] == '\0')
+    {
+        std::cout << "Tip : ./replace <file> <s1> <s2>" << std::endl;
+        std::cout << "<s1> and <s2> not empty." << std::endl;
+        return (1);
+    }
+    /*basic_ifstream();
+    
+        --only instantiation
+        explicit basic_ifstream(const char* s, ios_base::openmode mode = ios_base::in);
+        -- instantiation of the ifstream object and opening of file
+        explicit basic_ifstream(const string& s, ios_base::openmode mode = ios_base::in);
+        string str = "dir1/txtFile.txt";
+        ifstream ifs = ifstream(str, ios_base::in);
+    */
+    std::string infile(argv[1]);
+    std::ifstream   fin(infile);
+    if (fin.is_open() && std::string(*argv[2]).empty())
+    
     
 }
