5da59690f26423ceafb2d92c02026f995ba062db
Abla-ouh <ouhagaabla@gmail.com>
2022-12-08 11:37:59 +0100

    started ex05

---
+++ module01/ex05/Harl.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouhaga <abouhaga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/08 10:23:10 by abouhaga          #+#    #+#             */
+/*   Updated: 2022/12/08 10:30:15 by abouhaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+
+class Harl{
+    private:
+        void debug( void );
+        void info( void );
+        void warning( void );
+        void error( void );
+    public:
+        void complain( std::string level );
+};
+
+#endif
