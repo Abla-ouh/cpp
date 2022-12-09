7748ed44a4b9878af3b7b05ec9db5e0fde7e2cc6
Abla-ouh <Ouhagaabla@gmail.com>
2022-12-09 11:01:24 +0100

    test

---
+++ module01/ex05/Harl.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouhaga <abouhaga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/08 11:46:11 by abouhaga          #+#    #+#             */
+/*   Updated: 2022/12/08 11:46:29 by abouhaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+void	Harl::debug( void )
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void	Harl::info( void )
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+}
+
+void	Harl::warning( void )
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void	Harl::error( void )
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
