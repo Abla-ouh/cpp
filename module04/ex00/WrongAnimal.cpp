cd91871bc8bf24d9bc3a25646fef9dfd9e5dbe4d
Abla-ouh <Ouhagaabla@gmail.com>
2023-01-04 02:40:06 +0100

    fixed wrongAn

--- module04/ex00/WrongAnimal.cpp
+++ module04/ex00/WrongAnimal.cpp
@@ -6,7 +6,7 @@
 /*   By: abouhaga <abouhaga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 02:24:19 by abouhaga          #+#    #+#             */
-/*   Updated: 2023/01/04 02:24:42 by abouhaga         ###   ########.fr       */
+/*   Updated: 2023/01/04 02:39:35 by abouhaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,19 @@ WrongAnimal::WrongAnimal(const WrongAnimal &cpy)
 {
     std::cout << "WrongAnimal Copy constructor called" << std::endl;
     *this = cpy;
+}
+
+WrongAnimal &WrongAnimal::operator=(const WrongAnimal &oper)
+{
+    std::cout << "WrongAnimal Assignation operator called" << std::endl;
+    this->_type = oper._type;
+    return *this;
+}
+
+WrongAnimal::WrongAnimal(std::string type)
+{
+    std::cout << "WrongAnimal constructor called" << std::endl;
+    this->_type = type;
 }
 
 WrongAnimal &WrongAnimal::operator=(const WrongAnimal &oper)
